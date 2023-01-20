822cc36e0f46eb665b72c0a9c99ea87fa36314cc
Marcos <marcos.mrpb@gmail.com>
2023-01-20 10:43:57 -0300

    implementação do update

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -12,14 +12,15 @@
                 <i class="bi bi-check2-square fs-4"></i>
             </a>
             <h1>Editar Tarefa</h1>
-            <form action="/todos" method="post" class="mt-3">
+            <form action="/todos/{{ $todo->id }}" method="post" class="mt-3">
+                @method('PUT')
                 @csrf
                 <div class="input-group">
                     <div class="input-group-text bg-white p-0">
                          <!--<input type="color" class="form-control form-control-color border-0" name="color" title="Escolha uma cor"> -->
-                         <input type="color" class="form-control form-control-color border-0" id="color" title="Escolha uma cor">
+                         <input type="color" class="form-control form-control-color border-0" name="color" title="Escolha uma cor">
                     </div>
-                    <input type="text" class="form-control" name="title" placeholder="{{ $todo->title }}" required>
+                    <input type="text" class="form-control" name="title" value="{{ $todo->title }}" placeholder="   O que fazer?" required>
                     <button type="submit" class="btn btn-secondary">
                         <i class="bi bi-plus fs-4"></i>
                     </button>
